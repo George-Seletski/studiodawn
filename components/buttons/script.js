d2b1529526d8836b6706a97b79bd38563b5b6669
George  Seletski <george.seletski@gmail.com>
2023-05-22 00:25:02 +0200

    feat:added structure for the html and changed border

--- components/buttons/script.js
+++ components/buttons/script.js
@@ -1,5 +1,7 @@
 const buttonBox = document.querySelector('.button-box');
-
+var clicked_contact_button_flag = 0;
 buttonBox.addEventListener('click', function() {
     buttonBox.classList.toggle('clicked');
+    clicked_contact_button_flag += 1;
+
 });
